53772e0503be0412e70e6af1f60e30777fabfb09
tanyasrivastava24 <92261316+tanyasrivastava24@users.noreply.github.com>
2022-09-07 10:40:19 +0530

    Time: 179 ms (36.95%), Space: 15.8 MB (54.40%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.py
+++ 64-minimum-path-sum/64-minimum-path-sum.py
@@ -7,8 +7,6 @@ class Solution:
         for i in range(s):
             for j in range(t):
                 dp[i][j]=grid[i][j]
-        for i in range(s):
-            for j in range(t):
                 if i==0 and j>0:
                     dp[0][j]+=dp[0][j-1]
                 if j==0 and i>0:
@@ -16,3 +14,4 @@ class Solution:
                 if j>0 and i>0:
                     dp[i][j]+=min(dp[i-1][j],dp[i][j-1])
         return dp[s-1][t-1]
+    
